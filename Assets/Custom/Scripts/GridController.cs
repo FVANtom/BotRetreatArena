1b372464fd0d2823ad0608aaca3a27bcd744e437
johnny.hooyberghs <johnny.hooyberghs@prodatamobility.com>
2016-01-29 12:56:04 +0100

    Added name tags for bots
    Used a textmesh prefab and nametag script to attach nametag to bots

--- Assets/Custom/Scripts/GridController.cs
+++ Assets/Custom/Scripts/GridController.cs
@@ -2,129 +2,150 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace com.terranovita.botretreat {
+namespace com.terranovita.botretreat
+{
 
-  public class GridController : MonoBehaviour {
-
-    #region Singleton Code
-    /// <summary>
-    /// internal instance variable set in the Awake method 
-    /// </summary>
-    private static GridController _instance = null;
-
-
-    /// <summary>
-    /// The instance of the singleton. Should be only one in a scene. 
-    /// </summary>
-    public static GridController Instance
+    public class GridController : MonoBehaviour
     {
-      get
-      {
-        if (_instance == null)
+
+        #region Singleton Code
+        /// <summary>
+        /// internal instance variable set in the Awake method 
+        /// </summary>
+        private static GridController _instance = null;
+
+
+        /// <summary>
+        /// The instance of the singleton. Should be only one in a scene. 
+        /// </summary>
+        public static GridController Instance
         {
-          _instance = FindObjectOfType(typeof(GridController)) as GridController;
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType(typeof(GridController)) as GridController;
 
-          if (_instance == null)
-          {
-            GameObject go = new GameObject();
-            _instance = go.AddComponent<GridController>();
-          }
+                    if (_instance == null)
+                    {
+                        GameObject go = new GameObject();
+                        _instance = go.AddComponent<GridController>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+        public static bool exists()
+        {
+            return _instance != null;
         }
 
-        return _instance;
-      }
-    }
-    public static bool exists()
-    {
-      return _instance != null;
-    }
+        #endregion
 
-    #endregion
+        public GameObject gridPrefab;
+        public GameObject grid;
+        public float gridToWorldScale = 1;
+        public float platformHeight = 1;
+        public float refreshRate = 1;
+        private Arena arena;
 
-    public GameObject gridPrefab;
-    public GameObject grid;
-    public float gridToWorldScale = 1;
-    public float platformHeight = 1;
-    public float refreshRate = 1;
-    private Arena arena;
+        public GameObject botPrefab;
+        public GameObject nameTagPrefab;
+        public Dictionary<string, BotController> bots;
 
-    public GameObject botPrefab;
-    public Dictionary<string, BotController> bots;
-
-    private void initialize() {
-      if(grid == null) {
-        grid = Instantiate(gridPrefab);
-      }
-      grid.transform.localScale = new Vector3((float)arena.Width, platformHeight, (float)arena.Height);
-      grid.GetComponent<Renderer>().sharedMaterial.SetTextureScale("_MainTex", new Vector2((float)arena.Width, (float)arena.Height));
-      if(bots != null) {
-        foreach(var botId in bots.Keys) {
-          bots[botId].instantRefresh();
+        private void initialize()
+        {
+            if (grid == null)
+            {
+                grid = Instantiate(gridPrefab);
+            }
+            grid.transform.localScale = new Vector3((float)arena.Width, platformHeight, (float)arena.Height);
+            grid.GetComponent<Renderer>().sharedMaterial.SetTextureScale("_MainTex", new Vector2((float)arena.Width, (float)arena.Height));
+            if (bots != null)
+            {
+                foreach (var botId in bots.Keys)
+                {
+                    bots[botId].instantRefresh();
+                }
+            }
         }
-      }
-    }
 
-    public Vector3 gridToWorldPosition(int gridPositionX, int gridPositionY) {
-      if(arena != null) {
-      return new Vector3(
-        ((gridPositionX + gridToWorldScale/2 - ((float)arena.Width)/2 + gridPrefab.transform.position.x) * gridToWorldScale),
-        (platformHeight/2 * gridToWorldScale),
-        ((arena.Height - gridPositionY - gridToWorldScale/2) - ((float)arena.Height)/2 + gridPrefab.transform.position.z) * gridToWorldScale);
-      } else {
-        return Vector3.zero;
-      }
-    }
-
-    // Use this for initialization
-    void Start () {
-      InvokeRepeating("refreshGrid", 0, refreshRate);
-    }
-
-    void refreshGrid() {
-      Networking.Instance.refreshGrid(successCallback, errorCallback);
-    }
-
-    private void successCallback(JSONObject json)
-    {
-      //Debug.Log(json);
-      JSONObject arenaProperties = json.GetField("arena");
-      //Debug.Log(arenaProperties);
-      if(arenaProperties != null) {
-        Arena oldArena = arena;
-        arena = Arena.createFrom(arenaProperties);
-        if(oldArena == null || (oldArena.Width != arena.Width && oldArena.Height != arena.Height)) {
-          initialize();
+        public Vector3 gridToWorldPosition(int gridPositionX, int gridPositionY)
+        {
+            if (arena != null)
+            {
+                return new Vector3(
+                  ((gridPositionX + gridToWorldScale / 2 - ((float)arena.Width) / 2 + gridPrefab.transform.position.x) * gridToWorldScale),
+                  (platformHeight / 2 * gridToWorldScale),
+                  ((arena.Height - gridPositionY - gridToWorldScale / 2) - ((float)arena.Height) / 2 + gridPrefab.transform.position.z) * gridToWorldScale);
+            }
+            else {
+                return Vector3.zero;
+            }
         }
-      }
-      JSONObject botsJson = json.GetField("bots");
-      //Debug.Log(arenaProperties);
-      if(botsJson != null) {
-        refreshBots(botsJson);
-      }
-    }
-    private void errorCallback(JSONObject json)
-    {
-      Debug.Log(json.str);
-    }
 
-
-    private void refreshBots(JSONObject json) {
-      Dictionary<string, BotController> newBots = new Dictionary<string, BotController>();
-      foreach (JSONObject botJson in json.list)
-      {
-        Bot bot = Bot.createFrom(botJson);
-        BotController currentBot = null;
-        if(bots == null || !bots.TryGetValue(bot.Id, out currentBot)) {
-          GameObject currentBotGO = Instantiate(botPrefab);
-          currentBot = currentBotGO.GetComponent<BotController>();
+        // Use this for initialization
+        void Start()
+        {
+            InvokeRepeating("refreshGrid", 0, refreshRate);
         }
-        currentBot.bot = bot;
-        newBots.Add(bot.Id, currentBot);
-        //break;
-      }
-      bots = newBots;
+
+        void refreshGrid()
+        {
+            Networking.Instance.refreshGrid(successCallback, errorCallback);
+        }
+
+        private void successCallback(JSONObject json)
+        {
+            //Debug.Log(json);
+            JSONObject arenaProperties = json.GetField("arena");
+            //Debug.Log(arenaProperties);
+            if (arenaProperties != null)
+            {
+                Arena oldArena = arena;
+                arena = Arena.createFrom(arenaProperties);
+                if (oldArena == null || (oldArena.Width != arena.Width && oldArena.Height != arena.Height))
+                {
+                    initialize();
+                }
+            }
+            JSONObject botsJson = json.GetField("bots");
+            //Debug.Log(arenaProperties);
+            if (botsJson != null)
+            {
+                refreshBots(botsJson);
+            }
+        }
+        private void errorCallback(JSONObject json)
+        {
+            Debug.Log(json.str);
+        }
+
+
+        private void refreshBots(JSONObject json)
+        {
+            Dictionary<string, BotController> newBots = new Dictionary<string, BotController>();
+            foreach (JSONObject botJson in json.list)
+            {
+                Bot bot = Bot.createFrom(botJson);
+                BotController currentBot = null;
+                if (bots == null || !bots.TryGetValue(bot.Id, out currentBot))
+                {
+                    var currentBotGO = Instantiate(botPrefab);
+                    currentBot = currentBotGO.GetComponent<BotController>();
+                    var currentNameTagGO = Instantiate(nameTagPrefab);
+                    var txtMesh = currentNameTagGO.GetComponent<TextMesh>();
+                    txtMesh.text = bot.Name;
+                    currentNameTagGO.GetComponent<NameTagController>().BotGameObject = currentBotGO;
+                }
+                currentBot.bot = bot;
+                newBots.Add(bot.Id, currentBot);
+                //break;
+            }
+            bots = newBots;
+        }
+
+
     }
-
-
-  } 
 }
---
+++ Assets/Custom/Scripts/NameTagController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class NameTagController : MonoBehaviour
+{
+    private const float NAME_TAG_SCALE = 0.015f;
+
+    public GameObject BotGameObject { get; set; }
+
+    void Start()
+    {
+        Vector3 oldScale = transform.localScale;
+        transform.localScale = new Vector3(oldScale.x * NAME_TAG_SCALE, oldScale.y * NAME_TAG_SCALE, oldScale.z);
+    }
+
+    void Update()
+    {
+        // Position the name tag centered above the bot.
+        var botPosition = BotGameObject.transform.position;
+        transform.position = new Vector3(botPosition.x, 2, botPosition.z);
+        // Always let the name tags look directly at the camera.
+        var mainCameraRotation = Camera.main.transform.rotation;
+        transform.LookAt(transform.position + mainCameraRotation * Vector3.forward, mainCameraRotation * Vector3.up);
+    }
+}
